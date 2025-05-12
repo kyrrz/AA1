c77b794385810b7daa4cedc6e9fa0c9b0d0376a1
kyrrz <1999alvaroreyes@gmail.com>
2025-05-12 12:42:59 +0200

    cambios en backend

--- backend/src/controller/devsController.js
+++ backend/src/controller/devsController.js
@@ -5,12 +5,21 @@ const {
   modifyDev,
   removeDev,
 } = require("../service/devsService");
+
+const { getYears } = require("../dateUtils");
+const { getStudioValue } = require("../economyUtils");
 console.log("Dev controller loaded");
 //Operacion que devuelve todos los devs de la BBDD
 
 const getDevs = async (req, res) => {
   const data = await findAllDevs();
-
+  if (data === undefined) {
+    res.status(404).json({
+      status: "not-found",
+      message: "Devs not found",
+    });
+    return;
+  }
   res.status(200).json(data);
 };
 
@@ -18,6 +27,15 @@ const getDevs = async (req, res) => {
 
 const getDev = async (req, res) => {
   const data = await findDev(req.params.dev);
+  console.log(data);
+
+  if (data === undefined) {
+    res.status(404).json({
+      status: "not-found",
+      message: "Dev not found",
+    });
+    return;
+  }
 
   res.status(200).json(data);
 };
@@ -25,17 +43,113 @@ const getDev = async (req, res) => {
 //Operacion que registra un nuevo dev
 
 const postDev = async (req, res) => {
-  await registerDev(req.body.name, req.body.country, req.body.year);
+  if (req.body.name === undefined || req.body.name === "") {
+    res.status(400).json({
+      status: "bad-request",
+      message: "Dev name is required",
+    });
+    return;
+  }
 
-  res.status(201).json({});
+  if (req.body.country === undefined || req.body.country === "") {
+    res.status(400).json({
+      status: "bad-request",
+      message: "Dev country is required",
+    });
+    return;
+  }
+
+  if (
+    req.body.foundation_year === undefined ||
+    req.body.foundation_year === ""
+  ) {
+    res.status(400).json({
+      status: "bad-request",
+      message: "Dev year is required",
+    });
+    return;
+  }
+
+  const yearsActive = getYears(req.body.foundation_year);
+  const studioValue = getStudioValue(req.body.yearly_income, yearsActive);
+  let devId;
+
+  const returning = await registerDev(
+    req.body.name,
+    req.body.country,
+    req.body.foundation_year,
+    req.body.yearly_income,
+    yearsActive
+  );
+
+  res.status(201).json({
+    id: devId,
+    name: req.body.name,
+    country: req.body.country,
+    foundation_year: req.body.foundation_year,
+    yearsActive: yearsActive,
+    studioValue: studioValue,
+    status: "created",
+  });
 };
 
 //Operacion que modifica los datos de un dev en la BBDD
 
 const putDev = async (req, res) => {
-  await modifyDev(req.params.dev, req.body.country, req.body.year);
+  const devsFound = await findDev(req.params.dev);
 
-  res.status(204).json({});
+  if (!devsFound) {
+    res.status(404).json({
+      status: "not-found",
+      message: "Dev not found",
+    });
+    return;
+  }
+  if (req.params.dev === undefined || req.params.dev === "") {
+    res.status(400).json({
+      status: "bad-request",
+      message: "Dev name is required",
+    });
+    return;
+  }
+  if (req.body.country === undefined || req.body.country === "") {
+    res.status(400).json({
+      status: "bad-request",
+      message: "Dev country is required",
+    });
+    return;
+  }
+
+  if (
+    req.body.foundation_year === undefined ||
+    req.body.foundation_year === ""
+  ) {
+    res.status(400).json({
+      status: "bad-request",
+      message: "Dev year is required",
+    });
+    return;
+  }
+  const yearsActive = getYears(req.body.foundation_year);
+  const studioValue = getStudioValue(req.body.yearly_income, yearsActive);
+
+  const returning = await modifyDev(
+    req.params.dev,
+    req.body.country,
+    req.body.foundation_year,
+    req.body.yearly_income,
+    yearsActive
+  );
+
+  res.status(204).json({
+    id: returning[0].id,
+    name: req.body.name,
+    country: req.body.country,
+    foundation_yearyear: req.body.foundation_year,
+    yearsActive: yearsActive,
+    studioValue: studioValue,
+    status: "modified",
+  });
 };
 
 //Operacion que elimina un dev de la BBDD
--- backend/src/service/devsService.js
+++ backend/src/service/devsService.js
@@ -2,30 +2,54 @@ const db = require("../configuration/database").db;
 
 const findAllDevs = async () => {
   const result = await db("devs").select("*");
+
   return result;
 };
 
 const findDev = async (devName) => {
   const result = await db("devs").select("*").where({ name: devName }).first();
+  return result || null;
+};
+
+const registerDev = async (
+  devName,
+  devCountry,
+  devFoundationYear,
+  devYearlyIncome,
+  devYearsActive
+) => {
+  const result = await db("devs")
+    .insert({
+      name: devName,
+      country: devCountry,
+      foundation_year: devFoundationYear,
+      yearly_income: devYearlyIncome,
+      years_active: devYearsActive,
+    })
+    .then(async (ids) => {
+      devId = ids[0];
+    });
   return result;
 };
 
-const registerDev = async (devName, devCountry, devYear) => {
-  const result = await db("devs").insert({
-    name: devName,
-    country: devCountry,
-    year: devYear,
-  });
-  return result;
-};
-
-const modifyDev = async (devName, devCountry, devYear) => {
+const modifyDev = async (
+  devName,
+  devCountry,
+  devFoundationYear,
+  devYearlyIncome,
+  devYearsActive
+) => {
   const result = await db("devs")
     .where({
       name: devName,
     })
-    .update({ country: devCountry, year: devYear });
-
+    .update({
+      country: devCountry,
+      foundation_year: devFoundationYear,
+      yearly_income: devYearlyIncome,
+      years_active: devYearsActive,
+    })
+    .returning("id");
   return result;
 };
 
